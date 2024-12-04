14ee6e92ec36f0bad550579a82d60efa4f735c8b
terry <306746541@qq.com>
2024-12-04 23:48:44 +0800

    修改

--- a.c
+++ a.c
@@ -5,12 +5,17 @@
 #include <libavformat/avformat.h>
 
 // ffmpeg -re -i tnhaoxc.flv -c copy -f flv rtmp://192.168.0.104/live
+
 // ffmpeg -i rtmp://192.168.0.104/live -c copy tnlinyrx.flv
+
+// 打开本地文件，推流到流媒体服务
 // ./streamer tnhaoxc.flv rtmp://192.168.0.104/live
+//
+// 打开实时流，保存到文件
 // ./streamer rtmp://192.168.0.104/live tnhaoxc.flv
 int main(int argc, char **argv){
-    AVOutputFormat *ofmt = NULL;
-    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
+    AVOutputFormat *o_fmt = NULL;
+    AVFormatContext *i_fmt_ctx = NULL, *o_fmt_ctx = NULL;
     AVPacket pkt;
     const char *in_filename, *out_filename;
     int ret, i;
@@ -29,9 +34,9 @@ int main(int argc, char **argv){
                "\n", argv[0]);
         return 1;
     }
-    const char * ff_version = av_version_info();
+    const char * ff_version = av_version_info();// 打印ffmpeg版本号
     printf("ffmpeg_vserion:%s\n",ff_version);
-    unsigned _version=avformat_version();
+    unsigned _version=avformat_version();// 打印ffmpeg中格式化模块的版本号
     printf("vserion:%d\n",_version);
 
     in_filename  = argv[1];//输入流媒体地址
@@ -39,55 +44,55 @@ int main(int argc, char **argv){
 
     // 1. 打开输入
     // 1.1 读取文件头，获取封装格式相关信息
-    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+    if ((ret = avformat_open_input(&i_fmt_ctx, in_filename, 0, 0)) < 0) {
         printf("Could not open input file '%s'", in_filename);
         goto end;
     }
     
     // 1.2 解码一段数据，获取流相关信息
-    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+    if ((ret = avformat_find_stream_info(i_fmt_ctx, 0)) < 0) {
         printf("Failed to retrieve input stream information");
         goto end;
     }
 
-    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+    av_dump_format(i_fmt_ctx, 0, in_filename, 0);
 
     // 2. 打开输出
     // 2.1 分配输出ctx
     bool push_stream = false;
-    char *ofmt_name = NULL;
+    char *o_fmt_name = NULL;
     if (strstr(out_filename, "rtmp://") != NULL) {
         push_stream = true;
-        ofmt_name = "flv";
+        o_fmt_name = "flv";
     } else if (strstr(out_filename, "udp://") != NULL) {
         push_stream = true;
-        ofmt_name = "mpegts";
+        o_fmt_name = "mpegts";
     } else {
         push_stream = false;
-        ofmt_name = NULL;
+        o_fmt_name = NULL;
     }
-    avformat_alloc_output_context2(&ofmt_ctx, NULL, ofmt_name, out_filename);
-    if (!ofmt_ctx) {
+    avformat_alloc_output_context2(&o_fmt_ctx, NULL, o_fmt_name, out_filename);
+    if (!o_fmt_ctx) {
         printf("Could not create output context\n");
         ret = AVERROR_UNKNOWN;
         goto end;
     }
 
-    stream_mapping_size = ifmt_ctx->nb_streams;
+    stream_mapping_size = i_fmt_ctx->nb_streams;
     stream_mapping = av_mallocz_array(stream_mapping_size, sizeof(*stream_mapping));
     if (!stream_mapping) {
         ret = AVERROR(ENOMEM);
         goto end;
     }
 
-    ofmt = ofmt_ctx->oformat;
+    o_fmt = o_fmt_ctx->oformat;
 
     AVRational frame_rate;
     double duration;
 
-    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+    for (i = 0; i < i_fmt_ctx->nb_streams; i++) {
         AVStream *out_stream;
-        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVStream *in_stream = i_fmt_ctx->streams[i];
         AVCodecParameters *in_codecpar = in_stream->codecpar;
 
         if (in_codecpar->codec_type != AVMEDIA_TYPE_AUDIO &&
@@ -98,14 +103,14 @@ int main(int argc, char **argv){
         }
 
         if (push_stream && (in_codecpar->codec_type == AVMEDIA_TYPE_VIDEO)) {
-            frame_rate = av_guess_frame_rate(ifmt_ctx, in_stream, NULL);
+            frame_rate = av_guess_frame_rate(i_fmt_ctx, in_stream, NULL);
             duration = (frame_rate.num && frame_rate.den ? av_q2d((AVRational){frame_rate.den, frame_rate.num}) : 0);
         }
 
         stream_mapping[i] = stream_index++;
 
         // 2.2 将一个新流(out_stream)添加到输出文件(ofmt_ctx)
-        out_stream = avformat_new_stream(ofmt_ctx, NULL);
+        out_stream = avformat_new_stream(o_fmt_ctx, NULL);
         if (!out_stream) {
             printf("Failed allocating output stream\n");
             ret = AVERROR_UNKNOWN;
@@ -120,11 +125,11 @@ int main(int argc, char **argv){
         }
         out_stream->codecpar->codec_tag = 0;
     }
-    av_dump_format(ofmt_ctx, 0, out_filename, 1);
+    av_dump_format(o_fmt_ctx, 0, out_filename, 1);
 
-    if (!(ofmt->flags & AVFMT_NOFILE)) {    // TODO: 研究AVFMT_NOFILE标志
+    if (!(o_fmt->flags & AVFMT_NOFILE)) {    // TODO: 研究AVFMT_NOFILE标志
         // 2.4 创建并初始化一个AVIOContext，用以访问URL(out_filename)指定的资源
-        ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+        ret = avio_open(&o_fmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
         if (ret < 0) {
             printf("Could not open output file '%s'", out_filename);
             goto end;
@@ -133,7 +138,7 @@ int main(int argc, char **argv){
 
     // 3. 数据处理
     // 3.1 写输出文件头
-    ret = avformat_write_header(ofmt_ctx, NULL);
+    ret = avformat_write_header(o_fmt_ctx, NULL);
     if (ret < 0) {
         printf("Error occurred when opening output file\n");
         goto end;
@@ -143,12 +148,12 @@ int main(int argc, char **argv){
         AVStream *in_stream, *out_stream;
 
         // 3.2 从输出流读取一个packet
-        ret = av_read_frame(ifmt_ctx, &pkt);
+        ret = av_read_frame(i_fmt_ctx, &pkt);
         if (ret < 0) {
             break;
         }
 
-        in_stream  = ifmt_ctx->streams[pkt.stream_index];
+        in_stream  = i_fmt_ctx->streams[pkt.stream_index];
         if (pkt.stream_index >= stream_mapping_size ||
             stream_mapping[pkt.stream_index] < 0) {
             av_packet_unref(&pkt);
@@ -161,7 +166,7 @@ int main(int argc, char **argv){
         }
 
         pkt.stream_index = stream_mapping[pkt.stream_index];
-        out_stream = ofmt_ctx->streams[pkt.stream_index];
+        out_stream = o_fmt_ctx->streams[pkt.stream_index];
 
         /* copy packet */
         // 3.3 更新packet中的pts和dts
@@ -174,7 +179,7 @@ int main(int argc, char **argv){
         pkt.pos = -1;
 
         // 3.4 将packet写入输出
-        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+        ret = av_interleaved_write_frame(o_fmt_ctx, &pkt);
         if (ret < 0) {
             printf("Error muxing packet\n");
             break;
@@ -183,16 +188,16 @@ int main(int argc, char **argv){
     }
 
     // 3.5 写输出文件尾
-    av_write_trailer(ofmt_ctx);
+    av_write_trailer(o_fmt_ctx);
     printf("处理完成！\n");
 end:
-    avformat_close_input(&ifmt_ctx);
+    avformat_close_input(&i_fmt_ctx);
 
     /* close output */
-    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE)) {
-        avio_closep(&ofmt_ctx->pb);
+    if (o_fmt_ctx && !(o_fmt->flags & AVFMT_NOFILE)) {
+        avio_closep(&o_fmt_ctx->pb);
     }
-    avformat_free_context(ofmt_ctx);
+    avformat_free_context(o_fmt_ctx);
 
     av_freep(&stream_mapping);
 
